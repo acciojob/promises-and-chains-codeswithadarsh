0f8b51e55b3448013f8c476519159f59dc6e7d28
Adarsh Pandey <97337633+codeswithadarsh@users.noreply.github.com>
2023-10-09 09:24:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+var bttn=document.getElementById('btn');
+bttn.addEventListener("click",callby);
+    function promiseby(age,name){
+      return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+         if(age>=18){
+          resolve("Welcome, ${name}. You can vote.");}
+          else{
+           reject("Oh sorry ${name} You aren't old enough");
+          }
+        },1000)
+      })
+    }
+    function callby(){
+    var age=document.getElementById("age").value;
+    var name=document.getElementById('name').value;
+    promiseby(age,name)
+    .then((data)=>{
+      console.log("Success",data);
+    })
+    .catch((data)=>{
+      console.log("error",data);
+    })
+    }
